2123b6ab1bd919644033c96aa7750daa62a18054
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-05-10 20:35:13 +0200

    after the lesson

--- frontend/script.js
+++ frontend/script.js
@@ -1,101 +1,22 @@
-console.log(varVariable)
-
-let globalVariable1 = 12;
-
-let globalVariable2 = function () {
-    console.log("inside the function", globalVariable1);
-
-    let funVariable1 = "hello"
-
-    let funVariable2 = function () {
-        console.log("funVariable2 belseje")
-        console.log(globalVariable1)
-        console.log(funVariable1)
-
-        let funInFunVariable1 = true;
-        console.log(funInFunVariable1);
-
-        let funInFunVariable2 = function () {
-            console.log(funInFunVariable1);
-        }
-
-        funInFunVariable2();
-    }
-
-    funVariable2();
-
-    console.log(funVariable1);
-
-    if (funVariable1 === "hello") {
-        var blockVariable1 = "Bye"
-    } else {
-        /*console.log(blockVariable1);*/
-    }
-
-    console.log(blockVariable1);
+const menuButtonComponent = function () {
+    return `<button id="menu-btn"></button>`
 }
 
+const loadEvent = function () {
+    const rootElement = document.getElementById("root");
+    console.log(rootElement.parentElement);
 
-globalVariable2()
+    rootElement.insertAdjacentHTML("beforeend", menuButtonComponent());
 
-/*console.log(funVariable1);*/
+    const menuButtonEelement = document.getElementById("menu-btn")
 
-var varVariable = 1;
+    menuButtonEelement.addEventListener("click", function (event) {
+        //event.currentTarget.classList.toggle("clicked")
+        //console.log(rootElement);
+        //console.log(event.currentTarget.parentElement);
+        //console.log(event.currentTarget.closest(":root"));
 
-let letVariable1;
-console.log(letVariable1);
-
-let letVariable2 = "variable 2";
-console.log(letVariable2);
-
-const constVariable = "ciao";
-//constVariable = "hello";
-console.log(constVariable)
-
-const constObj1 = {};
-//constObj1.key = "value"; //ez működik, mert hozzáad értéket
-//constObj1 = { key: "value" } //ez nem fog működni, mert kicseréli a {}-et, új objektumot hoz létre
-console.log(constObj1);
-
-const globalVariable3 = function (parameter1, parameter2, parameter3) {
-    console.log(parameter1);
-    console.log(parameter2);
-    console.log(parameter3);
-    console.log(parameter3());
-}
-
-const globalVariable4 = "apple";
-
-const globalVariable5 = function () {
-    return "pear"
-}
-
-const globalVariable6 = function () {
-    return "shoes"
-}
-
-globalVariable3(globalVariable5(), globalVariable4, globalVariable6);
-
-
-
-
-
-
-
-
-
-
-
-const loadEvent = function (parameter1) {
-    const rootElement = document.getElementById("root")
-    console.log(rootElement)
-    console.log(parameter1);
-    //console.log(globalVariable1)
-
-    rootElement.addEventListener("click", function (event){
-        console.log(event.currentTarget);
-        event.currentTarget.insertAdjacentHTML("beforeend", "clicked,")
-        event.currentTarget.classList.toggle("clicked")
+        event.currentTarget.closest("#root").classList.toggle("menu-opened");
     })
 }
 
